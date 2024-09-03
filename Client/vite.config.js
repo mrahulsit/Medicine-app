96ce07ce814b05b48a617947ab11ad8e110ccfde
mrahulsit <mrahuls104@gmail.com>
2024-09-04 00:10:55 +0530

    Server File

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,22 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "ignore-missing-sourcemap",
-      enforce: "pre",
-      transform(code, id) {
-        if (id.includes("rsuite/Dropdown/styles/index.css")) {
-          return { code, map: null }; // Skip source map processing for this file
-        }
-      },
-    },
-  ],
-  build: {
-    sourcemap: false,
-  },
-  css: {
-    devSourcemap: false,
-  },
+  plugins: [react()],
 });
